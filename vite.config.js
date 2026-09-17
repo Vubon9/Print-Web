73f253eae138ea2edc373b5fb70bc5fb2e55f39c
Vubon <macbookair@Vubon-Mac.local>
2026-09-18 00:46:06 +0600

    update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Printer/",
+  // Remove: base: "/Printer/",
+  build: {
+    outDir: 'dist',
+  },
   server: {
     proxy: {
       "/api": {
